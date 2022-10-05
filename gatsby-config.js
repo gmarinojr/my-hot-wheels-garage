e826d82f77998b66e1f198dd38bbf7be4d6cc44b
Gary Marino Jr <garymarinojr@gmail.com>
2022-10-05 12:46:59 -0500

    built framework and added graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: `My Hot Wheels Garage`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [],
-}
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`
+      }
+    }
+  ]
+};
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,26 +1,43 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.css';
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <div className={container}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
-            <Link to='/' className={navLinkText}>
+            <Link to="/" className={navLinkText}>
               Home
             </Link>
           </li>
           <li className={navLinkItem}>
-            <Link to='/about' className={navLinkText}>
+            <Link to="/about" className={navLinkText}>
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
@@ -30,7 +47,7 @@ const Layout = ({ pageTitle, children }) => {
         {children}
       </main>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
